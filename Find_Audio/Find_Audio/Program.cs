5afef3f8062ec1924214dc52768a6de6b0f67859
bambooqj <bambooqj@163.com>
2023-03-20 11:47:56 +0800

    Add files via upload

---
+++ Find_Audio/Find_Audio/Program.cs
@@ -0,0 +1,22 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+        MMDevice device = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+        AudioSessionManager sessionManager = device.AudioSessionManager;
+        
+        for (int i = 0; i < sessionManager.Sessions.Count; i++)
+        {
+            AudioSessionControl sessionControl = sessionManager.Sessions[i];
+            uint processId = sessionControl.GetProcessID;
+            string processName = System.Diagnostics.Process.GetProcessById((int)processId).ProcessName;
+            Console.WriteLine("Process name: " + processName+"---ProcessID:"+processId);
+        }
+
+
+    }
+}
